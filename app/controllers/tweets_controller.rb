552ad55139ffab81a83ec7696f4f257a325b0ea0
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 17:18:46 +0000

    NoMethodError (undefined method `tweets' for #<User:0x00007f0550bf4c38>):

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
   
   def create
     tweet = Tweet.new(message: params[:tweet][:message])
-    user = User.find(session[:login_uid])
+    user = User.find_by(uid: session[:login_uid])
     user.tweets << tweet
     tweet.user = user
     if tweet.save
