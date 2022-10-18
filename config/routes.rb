2fd420e363c48bc96c522c11c8843fbe4e53f179
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 15:58:49 +0000

    Completed 200 OK in 80ms (Views: 71.3ms | ActiveRecord: 0.7ms)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'tweets/index'
   get 'tweets/new'
   post 'tweets/create'
-  delete 'tweets/:id', to: 'tweets#destroy'
+  delete '/:id', to: 'tweets#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
