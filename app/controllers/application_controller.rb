f014fdd249ca4323605ca8d074d17ea98a8095bd
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 16:35:36 +0000

    ActionController::RoutingError (undefined local variable or method `session' for ApplicationController:Class):

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    session[:c] ||= 0
+    session[:c] = session[:c].to_i + 1
+    render plain: session[:c]
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def main
     if session[:login_uid] != nil 
-      render main
+      render root_path
     else
       render 'login.html.erb'
     end
