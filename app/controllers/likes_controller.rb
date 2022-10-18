3c8b932d21f0e116337c648fc60239ca8c05d2fe
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 17:51:20 +0000

    ActionView::Template::Error (undefined method `use_id' for #<Like::ActiveRecord_Associations_CollectionProxy:0x00007f0550d483c8>):

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,8 @@ class LikesController < ApplicationController
     user = User.find_by(uid: session[:login_uid])
     user.like_tweets << tweet
     tweet.like_users << user
+    like = Like.new(user_id: user.id, tweet_id: tweet.id)
+    like.save
     redirect_to tweets_index_url
   end
   
