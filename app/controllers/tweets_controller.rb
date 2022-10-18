15d16a6bb4d947ed0d915d5014e3ee961d4e1c0e
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 15:55:45 +0000

    Completed 200 OK in 47ms (Views: 37.6ms | ActiveRecord: 0.6ms)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(message: params[:tweet][:message])
     if @tweet.save
-      render 'tweets/index'
+      redirect_to 'index'
     else
       return HttpResponse("ツイートの投稿に失敗しました")
     end
