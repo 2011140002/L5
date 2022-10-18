1ae8fab732d1128ce6a737afc201b85b3572fc3e
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 12:41:07 +0000

    Completed 200 OK in 1874ms (Views: 1848.7ms | ActiveRecord: 0.0ms)

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+  end
+  
+  def create
+    
+  end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :likes
+    has_many :like_tweets,
+    through: :likes,
+    source: :tweet
+    
+end
