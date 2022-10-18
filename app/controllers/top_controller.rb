04e8a87ad019cabc3e9444bfdfda1850d6f16e84
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 16:29:31 +0000

    SystemStackError (stack level too deep):

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def main
     if session[:login_uid] != nil 
-      render top_main
+      render main
     else
       render 'login.html.erb'
     end
