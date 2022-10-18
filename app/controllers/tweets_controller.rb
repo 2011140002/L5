0b6ac9d68ee87e177591cbc2ddf834eebbec96bd
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 15:51:48 +0000

    Completed 200 OK in 32ms (Views: 26.2ms | ActiveRecord: 0.0ms)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,13 +4,22 @@ class TweetsController < ApplicationController
   end
   
   def new
+    @tweet = Tweet.new
   end
   
   def create
-    
+    @tweet = Tweet.new(message: params[:tweet][:message])
+    if @tweet.save
+      render 'tweets/index'
+    else
+      return HttpResponse("ツイートの投稿に失敗しました")
+    end
   end
   
   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to 'tweets/index'
   end
   
   
