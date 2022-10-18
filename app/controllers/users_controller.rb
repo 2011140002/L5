ed142437c560957e01604788a59cb1980051b9c8
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-18 14:43:46 +0000

    NoMethodError (undefined method `empty?' for nil:NilClass):

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(uid: params[:uid], pass: params[:pass])
     if user.save
-      render "/"
+      render root_path
     else
       reder "error"
     end
