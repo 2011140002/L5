89e228cc677434e1b79cb7543085826dc74778cc
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-10-19 06:56:56 +0000

    Completed 200 OK in 58ms (Views: 57.1ms | ActiveRecord: 0.0ms)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
       session[:c] = session[:c].to_i + 1
       render plain: session[:c]
     end
+    
+    private
+    def current_user
+      if session[:login_uid]
+        User.find_by(uid: session[:login_uid])
+      end
+    end
+    helper_method :current_user
 end
